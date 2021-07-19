e5ec06bf1404f58038c9988de4bff023bfb8de95
Amogh Patil <63414678+amogh2510patil@users.noreply.github.com>
2021-07-19 15:36:34 +0530

    Add files via upload
    The arduino file that runs on the NodeMcu

---
+++ Euro_IOT/FirebaseDemo_ESP8266.ino
@@ -0,0 +1,71 @@
+//
+// Copyright 2015 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// FirebaseDemo_ESP8266 is a sample that demo the different functions
+// of the FirebaseArduino API.
+
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+// Set these to run example.
+#define FIREBASE_HOST "iot-test-62bb9-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "cqGqzWoVHx2rN3lGGfGz2Tvx1jPGzY5gT3bIcuPF"
+#define WIFI_SSID "GouriE903"
+#define WIFI_PASSWORD "Gouri@2911"
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.set("Led_Status",0);
+}
+
+int val = 0;
+
+void loop() {
+  // set value
+  val=Firebase.getInt("Led_Status");
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /number failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  else{
+    if(val==1){
+      digitalWrite(LED_BUILTIN,HIGH);
+      Serial.println("High");
+    }
+    else{
+      digitalWrite(LED_BUILTIN,LOW);
+      Serial.println("Low");
+    }
+  }
+  delay(1000);
+  
+ 
+}
